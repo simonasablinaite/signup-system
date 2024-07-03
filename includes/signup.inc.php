c8f77ea2f20fb74bea597fe6ef989001693fc530
Simona <simonasablinaite@gmail.com>
2024-07-03 12:23:24 +0300

    Error messages in signup

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -13,13 +13,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       require_once 'signup_contr.inc.php';
 
       // Klaidu tvarkykles:
+      $errors = [];
+
       if (is_input_empty($username, $pwd, $email)) {
+         $errors["empty_input"] = "Užpildykite visus laukelius!";
       }
       if (is_email_invalid($email)) {
+         $errors["invalid_email"] = "Naudojate neegzistuojantį el. paštą!";
       }
       if (is_username_taken($pdo, $username)) {
+         $errors["username_taken"] = "Naudotojo vardas jau užimtas!";
       }
       if (is_email_registered($pdo, $email)) {
+         $errors["email_used"] = "Vartotojas tokiu el. paštu jau registruotas!";
+      }
+
+      require_once 'config_session.inc.php';
+
+      if ($errors) {
+         $_SESSION["errors_signup"] = $errors;
+         header("Location: ../index.php");
       }
    } catch (PDOException $e) {
       die("Query failed: " . $e->getMessage());
